e8d820b9847544fd0ccbba7572d34f233c0d9e2c
Fernando Bruzzo <fernandobruzzo7@gmail.com>
2021-06-09 00:35:49 -0300

    resuelto primer ejercicio

--- PrimeraAplicacion/PrimeraAplicacion/Program.cs
+++ PrimeraAplicacion/PrimeraAplicacion/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace PrimeraAplicacion
 {
+    /*Cree una aplicación de consola 
+     * Solicite el ingreso de dos valores por pantalla 
+     * Al final muestre en forma descriptiva los resultados de aplicar las cuatro operaciones básicas
+     */
     class Program
     {
         static void Main(string[] args)
         {
+            int valor1, valor2;
+ 
+            valor1 = CargarNro();
+            valor2 = CargarNro();
+            Console.Clear();
+            Console.WriteLine($"La suma de {valor1} + {valor2} = {valor1 + valor2}");
+            Console.WriteLine($"La resta de {valor1} - {valor2} = {valor1 - valor2}");
+            Console.WriteLine($"La multiplicacion de {valor1} - {valor2} = {valor1 * valor2}");
+            Console.WriteLine($"La division de {valor1} / {valor2} = {valor1 / valor2}");
+            Console.ReadKey();
+        }
+
+        public static int CargarNro()
+        {
+            int nro;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("<-- Bienvenido -->");
+                Console.WriteLine("Ingrese un numero:");
+                int.TryParse(Console.ReadLine(), out nro);
+            } while (nro == 0);
+
+            return nro;
         }
     }
 }
