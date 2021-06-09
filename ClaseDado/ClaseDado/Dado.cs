89a07c03b3c3635b46615b7dd29a356489fcad79
Fernando Bruzzo <fernandobruzzo7@gmail.com>
2021-06-09 17:53:54 -0300

    Ejercicio ClaseDado completo

---
+++ ClaseDado/ClaseDado/Dado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseDado
+{
+    public class Dado
+    {
+        private int caras;
+        Random random = new Random();
+
+        public Dado(){}
+
+        public int getCaras() 
+        {
+            return this.caras;
+        }
+        public void lanzarDado()
+        {
+            this.caras = random.Next(1, 7);
+        }
+    }
+}
---
+++ ClaseDado/ClaseDado/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseDado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dado dado = new Dado();
+            Apostar(dado);
+        }
+        public static void Apostar(Dado dado)
+        {
+            int apuesta, seguir = 0;
+            String jugador;
+            do
+            {
+                Console.WriteLine("<--BIENVENIDOS AL JUEGO DE APUESTA-->\n" +
+                                "Por favor ingrese el valor");
+                apuesta = Int32.Parse(Console.ReadLine());
+
+                dado.lanzarDado();
+                Console.WriteLine($"El resultado del dado es {dado.getCaras()}");
+
+                jugador = (dado.getCaras() == apuesta) ? "Ganaste" : "Perdiste";
+                Console.WriteLine(jugador);
+
+                Console.WriteLine("Desea seguir apostando?\n" +
+                                    "1- seguir\n"+
+                                    "2- salir");
+                seguir = Int32.Parse(Console.ReadLine());
+                Console.Clear();
+            } while (seguir == 1);
+        }
+    }
+}
